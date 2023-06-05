caf447e8a2fa86e87236dd2bdc6f40e35476aa2d
sg-jaehyeon <jh.lee@supergate.cc>
2023-06-05 02:14:37 +0000

    early commit

--- target/debug/deps/dtb_patcher-ed743f09c5576706.d
+++
@@ -1,5 +0,0 @@
-/home/jaehyeon/dtb_patcher/target/debug/deps/dtb_patcher-ed743f09c5576706: src/main.rs
-
-/home/jaehyeon/dtb_patcher/target/debug/deps/dtb_patcher-ed743f09c5576706.d: src/main.rs
-
-src/main.rs:
--- target/debug/dtb_patcher.d
+++
@@ -1,1 +0,0 @@
-/home/jaehyeon/dtb_patcher/target/debug/dtb_patcher: /home/jaehyeon/dtb_patcher/src/main.rs
