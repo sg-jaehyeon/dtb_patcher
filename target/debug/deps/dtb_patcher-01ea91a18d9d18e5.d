3041b3651265c877072dcf6f85d08844148d684c
sg-jaehyeon <jh.lee@supergate.cc>
2023-06-19 17:30:37 +0900

    early commit

--- target/debug/deps/dtb_patcher-01ea91a18d9d18e5.d
+++
@@ -1,5 +0,0 @@
-/home/jaehyeon/dtb_patcher/target/debug/deps/dtb_patcher-01ea91a18d9d18e5.rmeta: src/main.rs
-
-/home/jaehyeon/dtb_patcher/target/debug/deps/dtb_patcher-01ea91a18d9d18e5.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/dtb_patcher-52c7184256fad6c1.d
+++
@@ -1,5 +0,0 @@
-/home/jaehyeon/dtb_patcher/target/debug/deps/dtb_patcher-52c7184256fad6c1.rmeta: src/main.rs
-
-/home/jaehyeon/dtb_patcher/target/debug/deps/dtb_patcher-52c7184256fad6c1.d: src/main.rs
-
-src/main.rs:
